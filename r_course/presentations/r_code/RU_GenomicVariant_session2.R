773141441cc76b1c718707441d5f65b4992f7f09
root <tc.infomatics@gmail.com>
2020-07-27 17:18:16 +0000

    Autobuild

---
+++ r_course/presentations/r_code/RU_GenomicVariant_session2.R
@@ -0,0 +1,19 @@
+params <-
+list(isSlides = "no")
+
+## ----include=FALSE------------------------------------------------------------
+suppressPackageStartupMessages(require(knitr))
+knitr::opts_chunk$set(echo = TRUE, tidy = T)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides != "yes"){
+  cat("# ATACseq (part 2)
+
+---
+"    
+  )
+  
+}
+
+
